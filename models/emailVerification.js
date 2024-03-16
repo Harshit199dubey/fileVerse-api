17616a90e7e0a6cbe9f891a87e0911ceee02534d
Lead Developer <harshit.dubey@walkingtree.tech>
2024-03-17 01:41:23 +0530

    testing husky

--- models/emailVerification.js
+++ models/emailVerification.js
@@ -3,13 +3,13 @@ const sequelize = require('./connection');
 
 const Verificationtable = sequelize.define('verificationtable', {
     email: {
-        type: DataTypes.STRING 
-    }, 
-    otpToken:{
-        type: DataTypes.STRING 
+        type: DataTypes.STRING
+    },
+    otpToken: {
+        type: DataTypes.STRING
     }
 }, {
     createdAt: 'created_at',
     updatedAt: 'updated_at'
-}); 
+});
 module.exports = Verificationtable;
