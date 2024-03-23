84b44a9968f80d87cb68c67169ee9dc9e6441975
Lead Developer <harshit.dubey@walkingtree.tech>
2024-03-24 03:47:03 +0530

    api

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const serverless = require("serverless-http");
 require("dotenv").config();
 
 const indexRouter = require("./routes/index");
@@ -18,5 +19,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/api", indexRouter);
 
 swaggerDocs(app, process.env.PORT);
-
+const handler = serverless(app);
+module.exports = handler;
 module.exports = app;
